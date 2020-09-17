c8c5ac604cb21da453b98e3551a07e2e4a81a2aa
watanabe nami <210392v@ugs.kochi-tech.ac.jp>
2020-09-17 10:09:26 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by 渡辺奈実 on 2020/09/17.
+//  Copyright © 2020 Watanabe nami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
